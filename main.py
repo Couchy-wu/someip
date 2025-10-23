d18d7a09d92b9d6f26b1a1dbbeab0d93dbd0b6e1
du.tong <du.tong@byd.com>
2025-10-23 14:10:35 +0800

    调整gui界面布局

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 import os
 from tkinter import ttk
+from tkinter import font as tkfont
 from GuiFunction.file_updater import FileUpdater
 from GuiFunction.file_handler import handle_file_upload
 from GuiFunction.delete_handler import delete_test_case
@@ -16,6 +17,9 @@ from test_can_gui import CANFDGUI
 root = tk.Tk()
 root.title("主窗口")
 root.geometry("800x600")
+
+# 定义一个的字体
+bold_font = tkfont.Font(family="微软雅黑", size=10, weight="normal")
 
 # 初始化文件更新器
 file_updater = FileUpdater()
@@ -34,16 +38,24 @@ video_processor = VideoProcessor(root)  # 传入主窗口
 upload_button = tk.Button(
     root,
     text="上传测试用例",
+    bg="#4A90E2",    # 蓝色背景
+    font=bold_font,
+    fg="white",      # 白色文字
+    activebackground="#357ABD",  # 按下时背景色
     command=handle_file_upload,
     width=15,
     height=2
 )
-upload_button.grid(row=0, column=0, padx=20, pady=20)
+upload_button.grid(row=0, column=0, padx=20, pady=20) 
 
 # 创建“删除测试用例”按钮
 delete_button = tk.Button(
     root,
     text="删除测试用例",
+    bg="#4A90E2",    # 蓝色背景
+    font=bold_font,
+    fg="white",      # 白色文字
+    activebackground="#357ABD",  # 按下时背景色
     command=delete_test_case,
     width=15,
     height=2
@@ -52,7 +64,7 @@ delete_button.grid(row=0, column=1, padx=20, pady=20)
 
 # 创建下拉菜单
 file_menu = ttk.OptionMenu(root, selected_file, *[])
-file_menu.grid(row=1, column=0, padx=20, pady=20)
+file_menu.grid(row=0, column=2, padx=20, pady=20)
 
 # 初始化下拉菜单
 file_updater.initialize_menu(selected_file, file_menu)
@@ -65,51 +77,71 @@ delete_button.config(command=lambda: file_updater.on_delete(selected_file, file_
 view_button = tk.Button(
     root,
     text="查看用例",
+    bg="#4A90E2",    # 蓝色背景
+    font=bold_font,
+    fg="white",      # 白色文字
+    activebackground="#357ABD",  # 按下时背景色
     command=view_case_handler.open_selected_file,
     width=15,
     height=2
 )
-view_button.grid(row=1, column=1, padx=20, pady=20)
+view_button.grid(row=0, column=3, padx=20, pady=20)
 
 # 添加“打开图片”按钮
 image_button = tk.Button(
     root,
     text="打开图片",
+    font=bold_font,
+    bg="#D9534F",
+    fg="white",
+    activebackground="#C9302C",
     command=image_handler.open_image,
     width=15,
     height=2
 )
-image_button.grid(row=2, column=0, padx=20, pady=20)
+image_button.grid(row=1, column=0, padx=20, pady=20)
 
-# 添加“读取视频”按钮
+# 添加“提取视频帧”按钮
 read_video_button = tk.Button(
     root,
-    text="读取视频",
+    text="提取视频帧",
+    font=bold_font,
+    bg="#D9534F",
+    fg="white",
+    activebackground="#C9302C",
     command=video_processor.process_video,
     width=15,
     height=2
 )
-read_video_button.grid(row=2, column=1, padx=20, pady=20)
+read_video_button.grid(row=1, column=1, padx=20, pady=20)
 
 # 添加“播放图片视频”按钮
 image_video_button = tk.Button(
     root,
     text="播放图片视频",
+    font=bold_font,
+    bg="#D9534F",
+    fg="white",
+    activebackground="#C9302C",
     command=GuiFunction.image_player.play_image_sequence,
     width=15,
     height=2
 )
-image_video_button.grid(row=3, column=0, columnspan=2, padx=20, pady=20)
+image_video_button.grid(row=1, column=2, padx=20, pady=20)
 
 # 添加“转换信号矩阵”按钮
 convert_matrix_button = tk.Button(
     root,
     text="转换信号矩阵",
+    font=bold_font,
+    bg="#5CB85C",
+    fg="white",
+    activebackground="#4CAE4C",
     command=lambda: open_matrix_converter(),
     width=15,
     height=2
 )
-convert_matrix_button.grid(row=0, column=2, columnspan=2, padx=20, pady=20)
+convert_matrix_button.grid(row=2, column=0, padx=20, pady=20)
 
 def open_matrix_converter():
     converter_window = tk.Toplevel(root)
@@ -124,11 +156,15 @@ def open_matrix_converter():
 hex_button = tk.Button(
     root,
     text="can数据生成器",
+    bg="#5CB85C",
+    fg="white",
+    font=bold_font,
+    activebackground="#4CAE4C",
     command=lambda: GuiFunction.binhex_gui.open_binhex_converter(root),
     width=15,
     height=2
 )
-hex_button.grid(row=1, column=2, padx=20, pady=20)  
+hex_button.grid(row=2, column=1, padx=20, pady=20)  
 
 # 添加“can测试”按钮，点击后调用test_can_gui.py
 can_window_instance = None      # 全局变量：用于存储子窗口实例
@@ -167,6 +203,10 @@ def open_can_gui():
 can_control_button = tk.Button(
     root,
     text="can测试",
+    bg="#5CB85C",
+    fg="white",
+    font=bold_font,
+    activebackground="#4CAE4C",
     command=open_can_gui,
     width=15,
     height=2
