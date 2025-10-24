0c660b18194ac3f30f2f5dece2ed6ec3e247d9d8
liu.kangjie1 <liu.kangjie1@byd.com>
2025-10-24 09:33:40 +0800

    1.添加网络框架结构信息显示

--- YOLO框架/ultralytics-8.3.217/train.py
+++ YOLO框架/ultralytics-8.3.217/train.py
@@ -1,6 +1,7 @@
 from ultralytics import YOLO
 import os
 import warnings
+
 warnings.filterwarnings('ignore')      # 可选：关闭不必要的警告
 
 # 必须在最顶部（在任何 import 之前）声明 start method（可选）
@@ -14,10 +15,13 @@ if __name__ == '__main__':
 
     model = YOLO('my_yolov8.yaml')  # 自定义结构
 
+    print('=== model (high‑level) ===')
+    print(model)  # 包含模型名称、训练/推理模式等信息
+    print('\n=== model.model (nn.Module) ===')
     # model = YOLO("yolov8m.pt")
-    model.train(data='coco128.yaml', epochs=100, imgsz=640, batch=16,amp = False, device=0)
+    # model.train(data='coco128.yaml', epochs=100, imgsz=640, batch=16,amp = False, device=0)
     # results = model.train(data="coco128.yaml", epochs=3)  # 训练模型
     # results = model.val()  # 在验证集上评估模型性能
     # results = model("https://ultralytics.com/images/bus.jpg")  # 预测图像
 
-    results = model.predict(source="Temptestimages/bus.jpg", save=True, imgsz=640, conf=0.25)
+    # results = model.predict(source="Temptestimages/bus.jpg", save=True, imgsz=640, conf=0.25)
